f26314c9f9da62a98c150a6dc562b0e8bbb1026e
mhamas <matej.hamas@gmail.com>
2025-02-20 20:44:51 +0100

    works

--- src/main.py
+++ src/main.py
@@ -1,185 +1,20 @@
 from __future__ import annotations
 
-from collections import namedtuple
 import logging
 
-from typing import TYPE_CHECKING, Annotated, Sequence, List, TypedDict
+from typing import TYPE_CHECKING
+
+from src.agents import State, data_gather_agent, scoring_agent, social_media_handle_finding_agent
 if TYPE_CHECKING:
     from langchain_core.runnables.config import RunnableConfig
 
-from langgraph.checkpoint.memory import MemorySaver
 from apify import Actor
-from langchain_core.messages import ToolMessage
-from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph, END
-
-from langchain.output_parsers import PydanticOutputParser
-from langgraph.graph.message import add_messages
 from src.llm import ChatOpenAISingleton
-from src.models import AgentStructuredOutput, EvidenceList, RawEvidenceList, SocialMediaHandles
+from src.models import AgentStructuredOutput
 from src.ppe_utils import charge_for_actor_start, charge_for_model_tokens, get_all_messages_total_tokens
-from src.tools import tool_person_name_to_social_network_handle, tool_scrape_instagram_profile_posts, tool_scrape_x_posts
 
-# Define state type
-class State(TypedDict):
-    """State of the agent graph."""
-
-    messages: Annotated[list, add_messages]
-    handles: SocialMediaHandles
-    name: str
-    rawEvidence: RawEvidenceList
-    evidence: EvidenceList
-    
-async def social_media_handle_finding_agent(state: State):
-    """Creates an agent that finds social media handles."""
-
-    Actor.log.debug('Running social media handle finding agent %s', state)
-
-    llm = ChatOpenAISingleton.get_instance()
-    llm_structured = llm.with_structured_output(SocialMediaHandles)
-
-    tools = [
-        tool_person_name_to_social_network_handle
-    ]
-    agent = create_react_agent(
-        llm,
-        tools,
-    )
-
-    messages = [
-        (
-            'user',
-            (
-                f"Find the social media handles for the person {state['name']}."
-            ),
-        )
-    ]
-
-    response: dict = {}
-    async for substate in agent.astream({'messages': messages}, stream_mode='values'):
-        message = substate['messages'][-1]
-        # traverse all tool messages and print them
-        if isinstance(message, ToolMessage):
-            # until the analyst message with tool_calls
-            for _message in substate['messages'][::-1]:
-                if hasattr(_message, 'tool_calls'):
-                    break
-                Actor.log.debug('-------- Tool --------')
-                Actor.log.debug('Message: %s', _message)
-            continue
-
-        Actor.log.debug('-------- Analyst --------')
-        Actor.log.debug('Message: %s', message)
-        response = substate
-    # response = await agent.ainvoke({'messages': messages})
-
-    Actor.log.debug('Social media handle finding agent response %s', response)
-    handles = llm_structured.invoke(response['messages'][-1].content)
-    
-    return {
-        "handles": handles,
-    }
-
-async def data_gather_agent(state: State):
-    """Creates the data gathering agent with social media tools."""
-    Actor.log.debug('Running data gathering agent %s', state)
-
-    tools = [
-        # tool_scrape_instagram_profile_posts,
-        tool_scrape_x_posts,
-    ]
-
-    llm = ChatOpenAISingleton.get_instance()
-    handles_prompt = ""
-    for handle in state["handles"].handles:
-        handles_prompt += f"{handle.socialMedia}: {handle.handle}\n"
-    
-    prompt = (
-        f"""
-            You are an agent that gathers evidence from social media.
-            
-            Use the social media handles from this mapping:
-            {handles_prompt}
-
-            Instructions:
-            1. Get 10 recent posts from each social network using person's handle.
-            2. For each social media, only use the corresponding handle from the mapping above.
-            3. If the handle for this social media is missing, skip this social media.
-            3. Combine all evidence into a single list
-            4. Do not filter or remove any evidence
-        """
-    )
-    messages = [ ( 'user', prompt) ]
-
-    agent = create_react_agent(
-        llm, 
-        tools,
-        response_format=RawEvidenceList
-    )
-
-    response = await agent.ainvoke({'messages': messages})
-    
-    Actor.log.debug('Data gathering agent response %s', response)
-
-    return {
-        "rawEvidence": response["structured_response"],
-    }
-
-async def scoring_agent(state: State):
-    """Creates the scoring agent."""
-    
-    Actor.log.debug('Running scoring agent %s', state)
-
-    llm = ChatOpenAISingleton.get_instance()
-    raw_evidence = state["rawEvidence"]
-
-    prompt = f"""Analyze the following pieces of evidence and score them based on how pro-western they are.
-    For each evidence, provide a score between -1.0 and 1.0 (inclusive), where:
-    - 1.0 represents strongly pro-western sentiment
-    - -1.0 represents strongly anti-western sentiment
-
-    Consider factors such as:
-    - Support for western democratic values
-    - Positive mentions of western countries, institutions, or leaders
-    - Alignment with western foreign policy positions
-    - Support for western economic systems
-
-    For each evidence, provide a relevance score between 0.0 and 1.0 (inclusive), where:
-    - 1.0 represents highly relevant evidence
-    - 0.0 represents not relevant at all
-
-    Score and relevance can be any floating point number with single decimal place, in the ranges defined above.
-
-    Evidence is relevant if it can be used to support the claim that the person is pro-western.
-
-    Evidence to analyze:
-    {[{"url": e.url, "text": e.text, "source": e.source} for e in raw_evidence.evidences]}
-
-    Return the results in the following JSON format:
-    {{"evidences": [
-        {{
-          "url": "evidence_url",
-          "text": "evidence_text",
-          "source": "evidence_source",
-          "score": 0.0 to 1.0
-          "relevance": 0.0 to 1.0
-        }},
-        ...
-    ]
-    }}
-
-    Url, text and source are always present in the evidence. Just copy them over to the output.
-    """
-    response = await llm.ainvoke(prompt)
-    
-    # Parse and validate the response
-    # Create the output parser to validate the structure
-    parser = PydanticOutputParser(pydantic_object=EvidenceList)
-    scored_evidences = parser.parse(response.content)
-
-    return {
-        "evidence": scored_evidences,
-    }
 
 async def main() -> None:
     """Main entry point for the Apify Actor.
@@ -242,16 +77,17 @@ async def main() -> None:
             if 'evidence' in state:
                 response = state['evidence']
         
-        if not response or not last_state:
+        if not response or not last_state or 'messages' not in last_state:
             Actor.log.error('Failed to get a response from the ReAct agent!')
             await Actor.fail(status_message='Failed to get a response from the ReAct agent!')
             return
-        
-        # if not (total_tokens := get_all_messages_total_tokens(messages)):
-        #     Actor.log.error('Failed to calculate the total number of tokens used!')
-        #     await Actor.fail(status_message='Failed to calculate the total number of tokens used!')
-        #     return
 
+        if not (total_tokens := get_all_messages_total_tokens(last_state["messages"])):
+            Actor.log.error('Failed to calculate the total number of tokens used!')
+            await Actor.fail(status_message='Failed to calculate the total number of tokens used!')
+            return
+
+        Actor.log.debug('Total tokens: %s', total_tokens)
         # await charge_for_model_tokens(model_name, total_tokens)
 
         result = response.model_dump() if response else {}
